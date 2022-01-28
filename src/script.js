af99f55f42b7d0ba1b796a3d69f769cab44f650c
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2022-01-27 22:00:07 -0600

    spherebuffer

--- src/script.js
+++ src/script.js
@@ -14,6 +14,7 @@ const scene = new THREE.Scene()
 
 // Objects
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// const sphereGeometry = new THREE.SphereBufferGeometry(.5, 64,64)
 
 // Materials
 
